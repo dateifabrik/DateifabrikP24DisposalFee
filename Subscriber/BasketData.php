ec772f040761d55ea19fef9555d440b60443d4a7
Dateifabrik <kontakt@dateifabrik.de>
2022-07-13 14:29:29 +0200

    eigenen Subscriber für BasketData implmentiert

---
+++ Subscriber/BasketData.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace DateifabrikP24DisposalFee\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+
+class BasketData implements SubscriberInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Checkout' => 'onPostDispatchFrontendCheckoutConfirm',
+        ];
+    }
+
+    public function onPostDispatchFrontendCheckoutConfirm(\Enlight_Event_EventArgs $args)
+    {
+
+        $actionName = $args->getSubject()->request()->getQuery('action');
+        if($actionName == 'confirm'){
+            echo "<p>\$args->getSubject()->getBasket()</p>";
+            dump($args->getSubject()->getBasket());
+            $basketContent = $args->getSubject()->getBasket()['content'];
+            $basketSessionID = $args->getSubject()->getBasket()['content'][0]['sessionID'];
+            $basketUserID = $args->getSubject()->getBasket()['content'][0]['userID'];
+
+            $i = 0;
+            foreach($basketContent as $basketItem){
+                echo "<p>\$args->getSubject()->getBasket()['content'][$i]</p>";
+                dump($basketItem);
+                $i++;
+            }
+
+            $sessionIdFromRepo = Shopware()->Models()->getRepository('Shopware\Models\Order\Basket');
+            // hole die Daten des Warenkorbs
+            echo "<p>\$sessionIdFromRepo->findBy(['sessionId' => \$basketSessionID])</p>";
+            dump($sessionIdFromRepo->findBy(['sessionId' => $basketSessionID]));
+
+            $userNameFromRepo = Shopware()->Models()->getRepository('Shopware\Models\Customer\Customer');
+            // finde die userId mit Hilfe des Repos
+            echo "<p>\$name = \$userNameFromRepo->find(\$basketUserID)->getFirstName(). ' '. \$userNameFromRepo->find(\$basketUserID)->getLastName()</p>";
+            dump($name = $userNameFromRepo->find($basketUserID)->getFirstName(). ' '. $userNameFromRepo->find($basketUserID)->getLastName());
+
+            // Shopware => Core => sBasket.php
+            // $module = Shopware()->Modules()->Basket();
+            // dump($module);
+
+            die();            
+
+        }
+
+    }
+
+
+
+}
--- Subscriber/TemplateRegistration.php
+++ Subscriber/TemplateRegistration.php
@@ -33,7 +33,6 @@ class TemplateRegistration implements SubscriberInterface
     {
         return [
             'Enlight_Controller_Action_PreDispatch_Frontend_Checkout' => 'onPreDispatch',
-            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Checkout' => 'onPostDispatchFrontendCheckoutConfirm',
         ];
     }
 
@@ -41,47 +40,5 @@ class TemplateRegistration implements SubscriberInterface
     {
         $this->templateManager->addTemplateDir($this->pluginDirectory . '/Resources/views');
     }
-
-    
-
-    public function onPostDispatchFrontendCheckoutConfirm(\Enlight_Event_EventArgs $args)
-    {
-
-        $actionName = $args->getSubject()->request()->getQuery('action');
-        if($actionName == 'confirm'){
-            echo "<p>\$args->getSubject()->getBasket()</p>";
-            dump($args->getSubject()->getBasket());
-            $basketContent = $args->getSubject()->getBasket()['content'];
-            $basketSessionID = $args->getSubject()->getBasket()['content'][0]['sessionID'];
-            $basketUserID = $args->getSubject()->getBasket()['content'][0]['userID'];
-
-            $i = 0;
-            foreach($basketContent as $basketItem){
-                echo "<p>\$args->getSubject()->getBasket()['content'][$i]</p>";
-                dump($basketItem);
-                $i++;
-            }
-
-            $sessionIdFromRepo = Shopware()->Models()->getRepository('Shopware\Models\Order\Basket');
-            // hole die Daten des Warenkorbs
-            echo "<p>\$sessionIdFromRepo->findBy(['sessionId' => \$basketSessionID])</p>";
-            dump($sessionIdFromRepo->findBy(['sessionId' => $basketSessionID]));
-
-            $userNameFromRepo = Shopware()->Models()->getRepository('Shopware\Models\Customer\Customer');
-            // finde die userId mit Hilfe des Repos
-            echo "<p>\$name = \$userNameFromRepo->find(\$basketUserID)->getFirstName(). ' '. \$userNameFromRepo->find(\$basketUserID)->getLastName()</p>";
-            dump($name = $userNameFromRepo->find($basketUserID)->getFirstName(). ' '. $userNameFromRepo->find($basketUserID)->getLastName());
-
-            // Shopware => Core => sBasket.php
-            // $module = Shopware()->Modules()->Basket();
-            // dump($module);
-
-            die();            
-
-        }
-
-    }
-
-
 
 }
