7fdb6f517d742620b705782adfe6ea35262c3ac0
Dateifabrik <kontakt@dateifabrik.de>
2022-11-06 09:33:21 +0100

    Erweiterung der Funktionsumfangs, Übernahme aus dem vorherigen Skript, weitere Tests

--- Subscriber/BasketData.php
+++ Subscriber/BasketData.php
@@ -7,26 +7,243 @@ use Enlight\Event\SubscriberInterface;
 class BasketData implements SubscriberInterface
 {
 
+    // checkout post dispatch
+
     public static function getSubscribedEvents()
     {
         return [
-            'Shopware_Modules_Basket_GetBasket_FilterItemStart' => 'filter',
+            'Enlight_Controller_Action_PreDispatch_Frontend_Checkout' => 'onBeforeCheckout',
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend_Checkout' => 'checkout',
+            'Shopware_Modules_Basket_getPriceForUpdateArticle_FilterPrice' => 'onUpdatePrice',
         ];
     }
 
-    public function filter(\Enlight_Event_EventArgs $args){
 
-        $test = $args->getReturn();
-        foreach($test as $t){
-            if($test['ordernumber'] == 11012){
-                $test['price'] = 555;
-                $test['netprice'] = $test['price']/1.19;
+
+    public function onBeforeCheckout(\Enlight_Event_EventArgs $args){
+
+        $subject = $args->getSubject();
+        $action = $subject->request()->getQuery('action');
+        $view = $subject->View();        
+        $countryId = Shopware()->Modules()->Admin()->sGetUserData()['billingaddress']['countryId'];        
+
+        // Formular anzeigen und Berechnungen nur für Action = confirm und Rechnungsadresse (countryId) Deutschland (2) ausführen        
+        if($action == 'confirm' AND $countryId == 2)
+        {
+
+            // Formular nur anzeigen, wenn countryId = 2 (Deutschland)
+            $view->assign('countryId', $countryId);         
+
+            // Ist eine Option für Entsorgungsgebühr ausgewählt?
+            // NULL = nichts ausgewählt
+            $licenseFeeOption = Shopware()->Session()->offsetGet('applyLicenseFeeOption');
+
+            if(isset($licenseFeeOption)){
+
+                // Option 1 => Ja
+                // Option 2 => Nein
+                switch($licenseFeeOption){
+                    case 1:
+                        $view->assign('selected1', 'selected="selected"');
+                        $view->assign('selected2', '');
+                        //$this->addLicenseFeeArticles($basketContent);
+                        $subject->get('session')->offsetSet('applyLicenseFeeOption', $licenseFeeOption);
+                        break;
+                    case 2:
+                        $view->assign('selected1', '');
+                        $view->assign('selected2', 'selected="selected"');
+                        //$this->removeLicenseFeeArticles($subject);
+                        $subject->get('session')->offsetSet('applyLicenseFeeOption', $licenseFeeOption);
+                        break;
+                }
             }
+
         }
-        return $test;
     }
 
 
+
+
+    public function checkout(\Enlight_Event_EventArgs $args){
+
+        $basketData = [];
+        $collectedContentData = [];
+
+        // get data from basket and store new price for disposal fee in session, so we can use it in onUpdatePrice()
+        $basketData = Shopware()->Modules()->Basket()->sGetBasketData();
+        //dump($basketData);
+
+        foreach($basketData['content'] as $content){
+
+            $collectedContentData[] = [
+                'basketId'              => $content['id'],
+                'articleId'             => $content['articleID'],
+                'ordernumber'           => $content['ordernumber'],
+                'quantity'              => $content['quantity'],                
+                'price'                 => $content['netprice'],
+                'tax_rate'              => $content['tax_rate'],
+                'p24_license_weight'    => $content['additional_details']['p24_license_weight'],
+                'p24_license_material'  => $content['additional_details']['p24_license_material'],
+                // used by materialHelper(), if p24_license_weight / p24_license_material empty or null
+                'p24_gewicht'          => $content['additional_details']['p24_gewicht'],
+                'p24_material'         => $content['additional_details']['p24_material'],
+            ];
+
+        }
+
+        //dump($collectedContentData);
+
+        $disposalFeeOrdernumberMaterialAndPrice = $this->calculateDisposalFeePrice($collectedContentData);
+        //dump($disposalFeeOrdernumberMaterialAndPrice);
+
+        //Shopware()->Session()->offsetSet('testBla', 888);
+
+    }
+
+    public function calculateDisposalFeePrice($collectedContentData){
+
+        $calculatedDisposalFeePrice = [
+            'ordernumberMaterial' => '',
+            'price' => ''
+        ];
+        
+        foreach($collectedContentData as $calcData){
+            // check if all used fields are filled, should not be empty
+            // if empty, return NULL or something helpful
+
+
+            if(empty($calcData['p24_license_material']) OR $calcData['p24_license_material'] == NULL){
+                //dump($calcData['p24_material']);
+                $material = $this->materialHelper($calcData['p24_material']);
+            }
+            else{
+                $material = $calcData['p24_license_material'];
+            }
+
+            // if material exists...
+            // else break here
+
+            //dump($material);
+            
+        }
+
+        return $calculatedDisposalFeePrice;
+    }
+
+    // is thrown only on putting article into basket
+    public function onUpdatePrice(\Enlight_Event_EventArgs $args){
+        $session = Shopware()->Session()->offsetGet('testBla');
+        //dump($session);
+
+        $basket = $args->getReturn();    
+        //dump($basket);
+        if($session == 888 AND $basket['ordernumber'] == 11012){
+            // set new price for disposal fee article
+            $basket['price'] = 1000 / 1.19; 
+        }
+
+        // return new values
+        //$args->setReturn($basket);
+    }
+
+
+
+
+    
+    // helper
+
+    public function addOrUpdateLicenseFeeArticles($args){
+
+        // wenn action = confirm
+        // case 1: noch kein lizenartikel im wk
+        // füge alle benötigten lizenzartikel hinzu
+        // case 2: es ist bereits mindestens ein lizenzartikel im wk
+        // update alle lizenzartikel
+
+        // wenn basket leer muss auch applyLicenseFeeOption auf 0 sein
+
+        $option = Shopware()->Session()->offsetGet('applyLicenseFeeOption');
+        dump($option);
+        //die();
+
+    }      
+
+    public function materialHelper($material){
+        
+        $alu = [];
+        $cardboard = [
+            'Karton',
+            'Kraft / PLA',
+            'Kraftpapier 50g / AL7 / CPP60',
+            'Kraftpapier 50g / PET12 / CPP40',
+            'PA / PE',
+            'Papier',
+            'Papier / PE',
+            'Papier / Pergament-Ersatz',
+            'Pappe',
+            'Pappe / PE',
+            'Pappe / PLA',
+            'Pappe / PP',
+        ];
+        $otherMaterial = [
+            'Bambus',
+            'Biokunststoff ( PLA )',
+            'Birke',
+            'CPLA',
+            'CPLA (Polymilchsäure)',
+            'Holz',
+            'Holz / Zellstoff',
+            'Mono-PP',
+            'Palmblatt',
+            'PLA',
+            'PLA ( Bio-Kunststoff )',
+            'Vlies',
+            'Zellulose / Maisstärke',
+            'Zellulose / PLA',
+            'Zuckerrohr',
+        ];
+        $plastic = [
+            'HDPE',
+            'Kunststoff ( LDPE )',
+            'Kunststoff ( PET )',
+            'Kunststoff ( PP )',
+            'Kunststoff ( PS )',
+            'Latex',
+            'Nitril',
+            'OPP25 / CPP40',
+            'OPPmatt19/AL7/PE110',
+            'PET',
+            'PET12 / AL7 / LDPE110',
+            'PET12 / LDPE110',
+            'PET12/AL7/LDPE110',
+            'Plastik',
+            'Plastik ( PET )',
+            'Plastik ( PP )',
+            'Plastik ( PS )',
+            'Plastik (PS)',
+            'PP',
+            'pp ( Polypropylen )',
+            'PS ( Polystyrol )',
+            'rPET',
+            'Styropor',
+        ];
+
+        if(in_array($material, $alu)){
+            $material = 'alu';
+        }
+        if(in_array($material, $cardboard)){
+            $material = 'cardboard';
+        }
+        if(in_array($material, $otherMaterial)){
+            $material = 'other_material';
+        }        
+        if(in_array($material, $plastic)){
+            $material = 'plastic';
+        }        
+
+        return $material;
+
+    }    
 
 
 }
